898d3059bb41b5010d14400139d62fcdc17c5040
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2025-07-06 19:22:08 +0200

    feat: add methods for passenger controller using constants

--- apps/api-gateway/src/passenger/passenger.controller.ts
+++ apps/api-gateway/src/passenger/passenger.controller.ts
@@ -1,9 +1,45 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { Body, Controller, Post, Get, Param, Put, Delete} from '@nestjs/common';
+import { PassengerDTO } from './dto/passenger.dto';
+import { Observable } from 'rxjs';
+import { PassengerMSG } from 'src/common/constants';
+import { IPassenger } from 'src/common/interfaces/passenger.interface';
 
 @Controller('api/v2/passenger')
 export class PassengerController {
   constructor(private readonly clientProxy: ClientProxyTrips) {}
   private _clientProxyPassenger = this.clientProxy.clientProxyPassengers();
 
-  
+  @Post()
+  create(@Body() passengerDTO: PassengerDTO): Observable<IPassenger> {
+    return this._clientProxyPassenger.send(PassengerMSG.CREATE, passengerDTO);
+  }
+
+  @Get()
+  findAll(): Observable<IPassenger[]> {
+    return this._clientProxyPassenger.send(PassengerMSG.FIND_ALL, {});
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Observable<IPassenger> {
+    return this._clientProxyPassenger.send(PassengerMSG.FIND_ONE, id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() passengerDTO: PassengerDTO,
+  ): Observable<IPassenger> {
+    return this._clientProxyPassenger.send(PassengerMSG.UPDATE, {
+      id,
+      passengerDTO,
+    });
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): Observable<IPassenger> {
+    return this._clientProxyPassenger.send(PassengerMSG.DELETE, id);
+  }
 }
