f67f04066529998fa62bb58a78092b50361fe6e8
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2025-07-06 18:17:29 +0200

    feat: add user controller methods

--- apps/api-gateway/src/user/user.controller.ts
+++ apps/api-gateway/src/user/user.controller.ts
@@ -1,16 +1,45 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { IUser } from 'src/common/interfaces/user.interface';
 import { ClientProxyTrips } from 'src/common/proxy/client-proxy';
 import { UserDTO } from './dto/user.dto';
+import { UserMSG } from 'src/common/constants';
+import { Observable } from 'rxjs';
 
 @Controller('user')
 export class UserController {
-  private _clientProxyUser = this.clientProxy.clientProxyUsers();
   constructor(private readonly clientProxy: ClientProxyTrips) {}
-  
+  private _clientProxyUser = this.clientProxy.clientProxyUsers();
 
   @Post()
   create(@Body() userDTO: UserDTO): Observable<IUser> {
-    return this._clientProxyUser.send()
+    return this._clientProxyUser.send(UserMSG.CREATE, userDTO);
+  }
+
+  @Get()
+  findAll(): Observable<IUser[]> {
+    return this._clientProxyUser.send(UserMSG.FIND_ALL, {});
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Observable<IUser> {
+    return this._clientProxyUser.send(UserMSG.FIND_ONE, id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() userDTO: UserDTO): Observable<IUser> {
+    return this._clientProxyUser.send(UserMSG.UPDATE, { id, userDTO });
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): Observable<IUser> {
+    return this._clientProxyUser.send(UserMSG.DELETE, id);
   }
 }
